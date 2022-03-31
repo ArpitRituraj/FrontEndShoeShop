452718d66f14cb3775672736bff013c825f347f7
Arpit Rituraj <riturajarpit23@gmail.com>
2022-04-01 01:55:11 +0530

    added Subtotal Feature in Cart

--- shoeshop/src/Components/Login.js
+++ shoeshop/src/Components/Login.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import Header from "./Header";
 import HeaderDown from "./HeaderDown";
 const Login=()=>{
@@ -15,13 +16,17 @@ const [price,totalPrice]=useState(456);
                   <img alt='Image' src="/images/2.png"></img>
                   <span>Nike Girl Shoe</span>
                   <span> Quantity
-                      <select onChange={(e)=>totalPrice((e.target.value)* price)}>
+                      <select onChange={(e)=>totalPrice((e.target.value)* 456)}>
                           <option>1</option>
                           <option>2</option>
                           <option>3</option>
                       </select>
                   </span>
                   <span>SUBTOTAL : ${price}</span>
+                  <Link to='/'>
+                  <button>CONTINUE TO SHOPPING</button>
+                  </Link>
+                  <div>CHECKOUT</div>
               </div>
         </div>
     )
